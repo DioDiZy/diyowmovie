eeef3a0be8c694427ad63aa5fd8caf62958f0aa2
DioDiZy <104825699+DioDiZy@users.noreply.github.com>
2023-06-21 17:47:05 +0700

    Add files via upload

---
+++ src/component/Pembukaan.js
@@ -0,0 +1,23 @@
+import { Container, Button, Row, Col } from "react-bootstrap";
+const Pembukaan = () => {
+  return (
+    <div>
+      <div className="Intro">
+        <Container className="text-white d-flex text justify-content-center align-item-center ">
+          <Row>
+            <Col>
+              <div className="tittle">Nonton Sepuasnya </div>
+              <div className="tittle">Hingga Kouta Tak Tersisa</div>
+              <a className="text-decoration-none" href="#daftarfilm">
+                <Button className="mt-4 d-flex m-auto" variant="dark">
+                  Lihat Film
+                </Button>
+              </a>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+export default Pembukaan;
